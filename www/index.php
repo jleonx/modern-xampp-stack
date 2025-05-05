4203a4c7417e9bef73b36e2c5dd89f5543eb87e0
Jose Luis Leon <jleon.x@gmail.com>
2025-05-05 11:20:58 -0500

    Add HTTPS redirection to index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,12 @@
-<?php phpinfo(); ?>
+<?php
+// Redirigir a HTTPS si no estamos ya en HTTPS
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+    $https_url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $https_url);
+    exit();
+}
+
+// Mostrar phpinfo() solo si es HTTPS
+phpinfo();
+?>
